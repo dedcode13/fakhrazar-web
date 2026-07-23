9b999607c08b7e7bfe8315c0f98f2bba37e8a450
dedcode13 <amirakhzari13@gmail.com>
2026-07-23 04:10:50 +0330

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,1 +1,39 @@
-export default function Home(){return <main>Fakhrazar Website</main>}
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
+
+import Hero from "@/sections/Hero";
+import Products from "@/sections/Products";
+import Industries from "@/sections/Industries";
+import Stats from "@/sections/Stats";
+import CTA from "@/sections/CTA";
+
+
+export default function Home(){
+
+return (
+
+<>
+
+<Navbar/>
+
+<main>
+
+<Hero/>
+
+<Products/>
+
+<Industries/>
+
+<Stats/>
+
+<CTA/>
+
+</main>
+
+<Footer/>
+
+</>
+
+)
+
+}
